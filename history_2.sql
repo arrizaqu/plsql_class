ccb1f66a3e2a2e6675a7d98e1f66fd4788598972
arrizaqu <arrizaqu@yahoo.com>
2017-05-03 16:35:03 +0700

    update history 2

--- history_2.sql
+++ history_2.sql
@@ -16,3 +16,76 @@ Tugas
 		 - setelah insert add_department berhasil 
 		 - masukkan data yang sama ke add_history
 - department_id(history_dept) = department_id(departments)  
+
+JAWAB :
+
+-- CREATE PROCEDURE TABLE HISTORY_DEPT
+CREATE OR REPLACE PROCEDURE create_table_history
+IS
+create_table varchar(1000);
+BEGIN
+  create_table := 'CREATE TABLE HR.HISTORY_DEPT
+  (
+    history_dept_id NUMBER,
+    department_name VARCHAR2(50),
+    location_id NUMBERs,
+    manager_id  NUMBER, department_id NUMBER
+  )';
+  EXECUTE IMMEDIATE create_table;  
+END create_table_history;
+/
+
+ 
+-- CREATE PROCEDURE add_deparments
+CREATE OR REPLACE PROCEDURE add_deparments(
+  p_name IN varchar2,
+  p_mgr IN varchar2,
+  p_loc IN number
+)
+IS
+v_gen_id NUMBER := departments_seq.NEXTVAL;
+BEGIN 
+  insert into departments(department_id, 
+				department_name, manager_id, location_id)
+  values 
+  (v_gen_id, p_name, p_mgr, p_loc);
+  DBMS_OUTPUT.PUT_LINE('SUCCESS ADD DEPARTMENT : ' || p_name);
+  add_history_dept_prc(p_name,p_loc,p_mgr,v_gen_id);
+EXCEPTION
+  WHEN OTHERS THEN
+    DBMS_OUTPUT.PUT_LINE('ERROR adding department : '|| p_name);
+END add_deparments;
+/
+
+-- CREATE PROCEDURE add_history_dept_prc
+CREATE OR REPLACE PROCEDURE add_history_dept_prc(
+  p_name hr.departments.DEPARTMENT_NAME%TYPE,
+  p_loc hr.departments.LOCATION_ID%TYPE,
+  p_mgr hr.departments.MANAGER_ID%TYPE,
+  p_dept_id NUMBER
+)
+IS
+BEGIN
+  INSERT INTO history_dept 
+    (HISTORY_DEPT_ID, DEPARTMENT_NAME, 
+    LOCATION_ID, MANAGER_ID, DEPARTMENT_ID)
+    values
+    (departments_seq.NEXTVAL, 
+					p_name, p_loc, p_mgr, p_dept_id);
+END add_history_dept_prc;
+/
+
+-- CALLING PROCEDURE add_deparments AND EXECUTION
+
+EXECUTE create_table_history;
+
+BEGIN
+  add_deparments('MEDIA', 100, 1800);
+  -- add_deparments('Editing', 99, 1800); -- ERROR
+  -- add_deparments('Advertising', 101, 1800);
+END;
+/
+
+-- CHECK TABLE DEPARTMENT and HISTORY_DEPT
+select * from departments where department_id = 
+select * from history_dept;
